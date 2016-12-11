1d5bda8590149867cbdd73c379531835e8c2fbc2
Pawel <Pawel@PAWEL-KOMPUTER>
2016-12-11 10:41:55 +0100

    Dodano podstawowa Klasę : "Osoba"

---
+++ Formularz-Projekt/Formularz-Projekt/Osoba.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Formularz_Projekt
+{
+    class Osoba
+    {
+        private string imie;
+        private string nazwisko;
+        private int numerTelefonu;
+        private string Email;
+        private string zawód;
+        private string typZawodu;
+        private string chorobyAlerg;
+        private int wiek;
+        private float masaCiała;
+        private float wzrost;
+        private float BMI;
+        private float ObwBioder;
+        private float ObwTalii;
+    }
+}
